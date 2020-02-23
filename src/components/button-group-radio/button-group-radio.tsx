f7f7c40f79a3080dcda6a8d0996e948ccf191eb8
gitmatlab <mat@alabaster.de>
2020-02-23 20:53:38 +0100

    second approach

--- src/components/button-group-radio/button-group-radio.tsx
+++ src/components/button-group-radio/button-group-radio.tsx
@@ -1,20 +1,41 @@
-import { Component, State, h } from '@stencil/core';
+import { Component, State, Listen, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'mat-button-group-radio',
-  styleUrl: 'button-group-radio.css',
+  styleUrl: 'button-group-radio.scss',
   shadow: true
 })
 
 export class ButtonGroupRadio {
+  @Prop({ reflect: true })
+  name: string
+
   @State()
-  value: string
+  checked: string
+
+  @Listen('click', { capture: true })
+  handleClick() {
+    const target = event.target as HTMLInputElement
+    this.checked = target.value
+    console.log(target, target.value)
+  }
+
+  hostData() {
+    return {
+      "role": "radiogroup",
+      "aria-labelledby": this.name,
+    }
+  }
+
+  childData() {
+    // role = "radio"
+    // "aria-checked" = "false" >
+  }
 
   render() {
-    console.log(this)
     return (
       <button-group-radio>
-        < slot ></slot >
+        <slot></slot>
       </button-group-radio>
     )
   }
